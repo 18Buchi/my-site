25458ebe7c9726664ca7f7c9581a1abb17e4f8e9
18Buchi <taka25803690@gmail.com>
2021-05-27 22:24:59 +0900

    ヘッダー編集

---
+++ svgIcon.js
@@ -0,0 +1,24 @@
+
+      $(function () {
+        $("<nav>").text("ww").appendTo("#header")
+
+
+      });
+
+    //   $("<img>")
+    //   .attr("src", items[i].src)
+    //   .attr("art", items[i].name)
+    //   .addClass(
+    //     "card-img-top shadow-sm p-3 mt-5 mb-1 bg-white rounded position-relative"
+    //   )
+    //   .appendTo(".c-items");
+    // $("<h5>")
+    //   .addClass("card-title d-inline-block w-50 mt-2")
+    //   .text("Card title")
+    //   .appendTo(".c-items");
+    // $("<a>")
+    //   .addClass("btn btn-secondary d-inline-block float-end")
+    //   .text("購入する!!")
+    //   .attr("href", items[i].link)
+    //   .appendTo(".c-items");
+    // // $(".card-img-top, .card-titl
