7220bd2dfff3aff0ed1fa777861b82b0715115f9
18Buchi <taka25803690@gmail.com>
2021-05-30 19:31:17 +0900

    APIテスト

---
+++ index.js
@@ -0,0 +1,29 @@
+const button = document.getElementById("addBtn");
+const lists = document.getElementById("lists");
+var vewNum = 5;
+
+// async function callApi() {
+// }
+// callApi();
+
+window.addEventListener("load", async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+  console.log(posts);
+
+  for (let index = 0; index < posts.length; index++) {
+    const post = posts[index];
+    if (post.id <= vewNum) {
+      const list = document.createElement("li");
+      list.innerText = post.title;
+      lists.appendChild(list);
+    }
+    // button.addEventListener("click", function () {
+    //   window.location.button();
+    // });
+  }
+  button.addEventListener("click", function () {
+    vewNum += 5;
+    console.log(vewNum);
+  });
+});
