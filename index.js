501bcbbc73a7d4977773e3396cd8023c16603c4c
18Buchi <taka25803690@gmail.com>
2021-06-01 22:16:14 +0900

    ✅ APIテスト0601

--- index.js
+++ index.js
@@ -2,23 +2,6 @@ const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists");
 var vewNum = 5;
 
-// async function callApi() {
-// }
-// callApi();
-button.addEventListener("click", async function () {
-  vewNum += 5;
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const posts = await res.json();
-  for (let index = vewNum -5; index < posts.length; index++) {
-    const post = posts[index];
-    if (post.id <= vewNum) {
-      const list = document.createElement("li");
-      list.innerText = post.title;
-      lists.appendChild(list);
-      console.log(vewNum);
-    }
-  }
-});
 
 window.addEventListener("load", async function () {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -28,11 +11,24 @@ window.addEventListener("load", async function () {
     if (post.id <= vewNum) {
       const list = document.createElement("li");
       list.innerText = post.title;
+      list.classList.add("list-wrapper");
       lists.appendChild(list);
     }
-    // button.addEventListener("click", function () {
-    //   window.location.button();
-    // });
   }
 });
 
+button.addEventListener("click", async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+  vewNum += 5;
+  for (let index = vewNum - 5; index < posts.length; index++) {
+    const post = posts[index];
+    if (post.id <= vewNum) {
+      const list = document.createElement("li");
+      list.innerText = post.title;
+      list.classList.add("list-wrapper");
+      lists.appendChild(list);
+      console.log(posts);
+    }
+  }
+});
