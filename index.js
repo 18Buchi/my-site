fad9084e110b400e4fb67434bc99934c4819c50c
18Buchi <taka25803690@gmail.com>
2021-06-02 22:14:36 +0900

    📝 Article追加

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
-const button = document.getElementById("addBtn");
+// const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists");
-var vewNum = 5;
-
 
 window.addEventListener("load", async function () {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await res.json();
   for (let index = 0; index < posts.length; index++) {
     const post = posts[index];
-    if (post.id <= vewNum) {
-      const list = document.createElement("li");
-      list.innerText = post.title;
-      list.classList.add("list-wrapper");
-      lists.appendChild(list);
-    }
-  }
-});
-
-button.addEventListener("click", async function () {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const posts = await res.json();
-  vewNum += 5;
-  for (let index = vewNum - 5; index < posts.length; index++) {
-    const post = posts[index];
-    if (post.id <= vewNum) {
-      const list = document.createElement("li");
-      list.innerText = post.title;
-      list.classList.add("list-wrapper");
-      lists.appendChild(list);
-      console.log(posts);
-    }
+    const list = document.createElement("li");
+    list.innerText = post.title;
+    list.setAttribute("class", "list-wrapper");
+    list.setAttribute("id", post.id);
+    lists.appendChild(list);
   }
 });
