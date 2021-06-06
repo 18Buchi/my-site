0d7daa2a25d63456c778a985492d524c6236329d
18Buchi <taka25803690@gmail.com>
2021-06-06 16:26:25 +0900

    💄 モーダルウインド修正

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists");
 const close = document.getElementById("close");
 const popUpTitle = document.getElementById("popup-title");
 const popUpText = document.getElementById("popup-text");
 const popCard = document.getElementById("c-card");
+const container = document.getElementById("c-container");
 
 window.addEventListener("load", async function () {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -15,21 +15,20 @@ window.addEventListener("load", async function () {
     list.setAttribute("class", "list-wrapper");
     list.setAttribute("id", post.id);
     lists.appendChild(list);
-
     const listId = document.getElementById(post.id);
     listId.addEventListener("click", async function (e) {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
       const posts = await res.json();
       let getId = e.target.id;
       const post = posts[getId - 1];
-
       popCard.classList.add("c-show-card");
+      container.classList.add("c-conNone");
       popUpTitle.innerText = post.title;
       popUpText.innerText = post.body;
     });
   }
 });
-
 close.addEventListener("click", function () {
   popCard.classList.remove("c-show-card");
+  container.classList.remove("c-conNone");
 });
