93a2246d3cdfdb5ce2350265f16e128f4312536f
18Buchi <taka25803690@gmail.com>
2021-09-07 22:44:54 +0900

    🚧 footer調整

--- footer.js
+++ footer.js
@@ -1,34 +1,107 @@
 "use strict";
 {
+  // トップボタン
   const footer = document.getElementById("footer");
-  // footer.innerText = "sssss";
   footer.style.color = "white";
   footer.style.backgroundColor = "#023178";
-  footer.style.height = "200px";
+  footer.style.height = "250px";
   footer.style.padding = "30px";
 
   let topBtn = document.createElement("a");
   topBtn.innerHTML = "Top Button";
   footer.appendChild(topBtn);
   topBtn.classList.add("btn", "btn-light");
+  topBtn.style.fontWeight = "bold";
 
+  // 外部サイトアイコン
   let nav = document.createElement("nav");
   let ul = document.createElement("ul");
-  let li = document.createElement("li");
 
-  li.innerHTML = "1";
   footer.appendChild(nav);
   nav.appendChild(ul);
-  ul.appendChild(li);
-}
 
-// "btn btn-secondary d-inline-block float-end btn-sm  mt-1"
-// const photos = await res2.json();
-//       let getId = e.target.id;
-//       const post = posts[getId - 1];
-//       const photo = photos[getId - 1];
-//       popCard.classList.add("c-show-card");
-//       container.classList.add("c-conNone");
-//       popUpTitle.innerText = post.title;
-//       popUpText.innerText = post.body;
-//       image.setAttribute("src", photo.url);
+  let footerIcon = [
+    {
+      src: "./image/icon/icon001.jpg",
+      title: "1",
+      link: "#",
+    },
+    {
+      src: "./image/icon/icon002.jpg",
+      title: "2",
+      link: "#",
+    },
+    {
+      src: "./image/icon/icon003.jpg",
+      title: "2",
+      link: "#",
+    },
+    {
+      src: "./image/icon/icon004.jpg",
+      title: "2",
+      link: "#",
+    },
+  ];
+  for (let i = 0; i < footerIcon.length; i++) {
+    let li = document.createElement("li");
+    let icon = document.createElement("img");
+    icon.src = footerIcon[i].src;
+    ul.appendChild(li);
+    li.appendChild(icon);
+    icon.style.width = "30px";
+    icon.style.marginTop = "2vh";
+    icon.style.marginRight = "2vw";
+    icon.style.borderRadius = "7px";
+    icon.style.float = "left";
+    li.style.listStyle = "none";
+    
+  }
+  ul.style.margin = "0px";
+  ul.style.padding = "0px";
+  ul.style.display = "inline";
+
+  
+
+  // フォーム
+  let ulForm = document.createElement("ul");
+  
+
+
+  footer.appendChild(ulForm);
+  
+
+  let form = [
+    {
+      src: "#",
+      title: "お問合せ",
+      link: "#",
+    },
+    {
+      src: "#",
+      title: "免責事項",
+      link: "#",
+    },
+    {
+      src: "#",
+      title: "Copy",
+      link: "#",
+    },
+  ];
+
+  for (let i = 0; i < form.length; i++) {
+    let liForm = document.createElement("li");
+    let linkForm = document.createElement("a");
+    liForm.innerHTML = form[i].title;
+    ulForm.appendChild(liForm);
+    liForm.style.float = "left";
+    liForm.style.paddingLeft = "2vw";
+    liForm.style.listStyle = "none";
+  }
+  ulForm.style.padding = "0px";
+  // ulForm.style.position = "relative";
+  ulForm.style.float = "right";
+  ulForm.style.marginTop = "80px";
+  ulForm.style.marginRight = "2vw";
+  // ulForm.style.marginLeft = "60vw";
+  
+}
